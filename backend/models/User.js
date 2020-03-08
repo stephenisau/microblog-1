81b2838b63c7d2a4ecb58cfb6153d685abfeb7a6
stephenau93 <stephenau93@gmail.com>
2020-03-07 22:46:50 -0800

    Fix typo in backend on User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -49,91 +49,89 @@ class User {
     return result.rows[0];
   }
 
-    /** Given a username, return data about user. */
+  /** Given a username, return data about user. */
 
-    static async findOne(username) {
-      const userRes = await db.query(
-          `SELECT username, first_name, last_name, email, photo_url 
+  static async findOne(username) {
+    const userRes = await db.query(
+      `SELECT username, first_name, last_name, email, photo_url 
               FROM users 
               WHERE username = $1`,
-          [username]);
-  
-      const user = userRes.rows[0];
-  
-      if (!user) {
-        const error = new Error(`There exists no user '${username}'`);
-        error.status = 404;   // 404 NOT FOUND
-        throw error;
-      }
-  
-      const userJobsRes = await db.query(
-          `SELECT j.id, j.title, j.company_handle, a.state 
+      [username]);
+
+    const user = userRes.rows[0];
+
+    if (!user) {
+      const error = new Error(`There exists no user '${username}'`);
+      error.status = 404;   // 404 NOT FOUND
+      throw error;
+    }
+
+    const userJobsRes = await db.query(
+      `SELECT j.id, j.title, j.company_handle, a.state 
              FROM applications AS a
                JOIN jobs AS j ON j.id = a.job_id
              WHERE a.username = $1`,
-          [username]);
-  
-      user.jobs = userJobsRes.rows;
-      return user;
+      [username]);
+
+    user.jobs = userJobsRes.rows;
+    return user;
+  }
+
+  /** Update user data with `data`.
+   *
+   * This is a "partial update" --- it's fine if data doesn't contain
+   * all the fields; this only changes provided ones.
+   *
+   * Return data for changed user.
+   *
+   */
+
+  static async update(username, data) {
+    if (data.password) {
+      data.password = await bcrypt.hash(data.password, BCRYPT_WORK_FACTOR);
     }
-  
-    /** Update user data with `data`.
-     *
-     * This is a "partial update" --- it's fine if data doesn't contain
-     * all the fields; this only changes provided ones.
-     *
-     * Return data for changed user.
-     *
-     */
-  
-    static async update(username, data) {
-      if (data.password) {
-        data.password = await bcrypt.hash(data.password, BCRYPT_WORK_FACTOR);
-      }
-  
-      let {query, values} = partialUpdate(
-          "users",
-          data,
-          "username",
-          username
-      );
-  
-      const result = await db.query(query, values);
-      const user = result.rows[0];
-  
-      if (!user) {
-        let notFound = new Error(`There exists no user '${username}`);
-        notFound.status = 404;
-        throw notFound;
-      }
-  
-      delete user.password;
-      delete user.is_admin;
-  
-      return result.rows[0];
+
+    let { query, values } = partialUpdate(
+      "users",
+      data,
+      "username",
+      username
+    );
+
+    const result = await db.query(query, values);
+    const user = result.rows[0];
+
+    if (!user) {
+      let notFound = new Error(`There exists no user '${username}`);
+      notFound.status = 404;
+      throw notFound;
     }
-  
-    /** Delete given user from database; returns undefined. */
-  
-    static async remove(username) {
-        let result = await db.query(
-                `DELETE FROM users 
+
+    delete user.password;
+    delete user.is_admin;
+
+    return result.rows[0];
+  }
+
+  /** Delete given user from database; returns undefined. */
+
+  static async remove(username) {
+    let result = await db.query(
+      `DELETE FROM users 
                   WHERE username = $1
                   RETURNING username`,
-                [username]);
-  
-      if (result.rows.length === 0) {
-        let notFound = new Error(`There exists no user '${username}'`);
-        notFound.status = 404;
-        throw notFound;
-      }
+      [username]);
+
+    if (result.rows.length === 0) {
+      let notFound = new Error(`There exists no user '${username}'`);
+      notFound.status = 404;
+      throw notFound;
     }
   }
-  
-  
-  module.exports = User;
-
-
-
-
 }
+
+
+module.exports = User;
+
+
+
