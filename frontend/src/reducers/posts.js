7cc04f649193a0755860e1516de30f9d1507d7c3
stephenau93 <stephenau93@gmail.com>
2020-03-18 21:25:08 -0700

    refactored reducers and thunk action creators to include payload as top-level key for data

--- frontend/src/reducers/posts.js
+++ frontend/src/reducers/posts.js
@@ -11,47 +11,51 @@ import {
 
 
 export default function rootReducer(state = {}, action) {
-  let p = state[action.postId];
+  let p;
 
   switch (action.type) {
 
     case FETCH_POST:
-      return { ...state, [action.post.id]: action.post };
+      debugger;
+      return { ...state, [action.payload.post.id]: action.payload.post };
 
     case ADD_POST:
-      return { ...state, [action.post.id]: { ...action.post, comments: [] }};
+      return { ...state, [action.payload.post.id]: { ...action.payload.post, comments: [] }};
 
     case UPDATE_POST:
       return {
         ...state,
-        [action.post.id]: {
-          ...action.post,
-          comments: state[action.post.id].comments
+        [action.payload.post.id]: {
+          ...action.payload.post,
+          comments: state[action.payload.post.id].comments
         }
       };
 
     case REMOVE_POST:
       let posts = { ...state };
-      delete posts[action.postId];
+      delete posts[action.payload.postId];
       return posts;
 
     case VOTE:
+      p = state[action.payload.postId];
       return {
         ...state,
-        [action.postId]: { ...p, votes: action.votes }
+        [action.payload.postId]: { ...p, votes: action.payload.votes }
       };
 
     case ADD_COMMENT:
+      p = state[action.payload.postId];
       return {
         ...state,
-        [action.postId]: { ...p, comments: [...p.comments, action.comment] }
+        [action.payload.postId]: { ...p, comments: [...p.comments, action.payload.comment] }
       };
 
     case REMOVE_COMMENT :
+      p = state[action.payload.postId];
       return {
         ...state,
-        [action.postId]: {
-          ...p, comments: p.comments.filter(c => c.id !== action.commentId)
+        [action.payload.postId]: {
+          ...p, comments: p.comments.filter(c => c.id !== action.payload.commentId)
         }
       };
 
